a7cb29efe33d056ac83a28a4af393f6eac6bf7e9
pksatyam777 <pksatyam777@gmail.com>
2023-08-26 12:51:50 +0530

    added profile on right side

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -5,7 +5,7 @@ import {AiFillHome} from 'react-icons/ai'
 import {FiPlusCircle} from 'react-icons/fi'
 export default function Header() {
   return (
-    <header className="flex items-center justify-between px-8 bg-white">
+    <header className="flex items-center justify-between px-48 bg-white">
         <div className="h-24 w-24 relative hidden lg:inline-grid cursor-pointer">
             <Image className="object-contain" layout='fill' src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Instagram_logo.svg/1280px-Instagram_logo.svg.png" />
         </div>
@@ -17,9 +17,9 @@ export default function Header() {
            <input type="text" placeholder="search..." className="bg-gray-50 rounded-lg pl-12 border-gray-500 focus:ring-black focus:border-black  text" />
         </div>
         <div className=" flex justify-between space-x-6 items-center">
-            <AiFillHome className="text-2xl" />
-            <FiPlusCircle className="text-2xl"/>
-            <Image height="40" width="40" className=""  src="https://www.svgrepo.com/show/129839/avatar.svg" /> 
+            <AiFillHome className="text-2xl transform transition-transform hover:scale-125 duration-200 ease-out hidden lg:inline-grid" />
+            <FiPlusCircle className="text-2xl transform transition-transform hover:scale-125 duration-200 ease-out"/>
+            <Image height="40" width="40" className=""  src="https://www.svgrepo.com/show/129839/avatar.svg" alt="user-image"/> 
         </div>
     </header>
   )
